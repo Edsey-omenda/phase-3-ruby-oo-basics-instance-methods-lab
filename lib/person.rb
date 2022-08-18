7eeeb66cabfbf5b9e2b8e0ea3b3c70ac7fb48bc9
Edsey omenda <omendajames0317@gmail.com>
2022-08-18 23:19:42 +0300

    completed

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def  walk
+        puts   "The Person is walking"
+    end
+end
+
+James = Person.new
+James.talk
+Edsey = Person.new
+Edsey.walk
